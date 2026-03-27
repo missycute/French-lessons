6b6500d93d8aece1722fc8bfde9797e56868029f
missycute <zaydah2501@gmail.com>
2026-03-27 21:04:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -447,9 +447,4 @@ copyRoomBtn.addEventListener("click", async function () {
 startRealtimeListener();
 renderEntries();
 renderStudyCard();
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
